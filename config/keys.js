20e14a3feb36edd2db5b6149bb16490d55e3508c
stevenjoeng <stevenswjoeng@gmail.com>
2020-10-19 14:35:51 +0900

    commit

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,10 @@
 module.exports = {
-
+    mongodb: {
+        user: "tester",
+        password: "testerPassword"
+    },
+    google: {
+        gmail: "counselor.unofficial@gmail.com",
+        password: "counselor2020!"
+    }
 };
