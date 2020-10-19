38e1367845e583addfea39b3b969ae41f010e6a0
seungw0n <34816905+seungw0n@users.noreply.github.com>
2020-10-19 14:36:20 +0900

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,10 +1,3 @@
 module.exports = {
-    mongodb: {
-        user: "tester",
-        password: "testerPassword"
-    },
-    google: {
-        gmail: "counselor.unofficial@gmail.com",
-        password: "counselor2020!"
-    }
+    
 };
