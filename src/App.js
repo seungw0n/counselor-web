5711155cab26dfd83c5833b5a704fdf6c7615fb5
wnwkr13 <wnwkr13@gmail.com>
2020-10-30 19:12:47 +0900

    work

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Fragment } from 'react';
 import './App.css';
 import {BrowserRouter as Router } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
+import About from './components/About/About';
 import Banner from './components/banner/banner';
 import AIConsulting from './components/AIConsulting/AIConsulting'
 import Footer from './components/Footer/Footer'
@@ -14,6 +15,7 @@ function App() {
     <Router>
       <Fragment>
         <NavBar/>
+        <About/>
         <Banner/>
         <AIConsulting/>
         <Footer/>
--- src/components/AIConsulting/AIConsulting.js
+++ src/components/AIConsulting/AIConsulting.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import '../../../node_modules/materialize-css/dist/css/materialize.min.css';
 
 export default class AIConsulting extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <section id="AIConsulting" className="section section-icons grey lighten-4 center">
+                <div className="container">
+                     
+                </div>
+            </section>
         )
     }
 }
